76a0682eaeacd500382e4e95cfb719f66bb76426
Kahee Tomlinson <48109534+EvaTomlinson@users.noreply.github.com>
2022-02-01 19:20:44 -0500

    added router

--- website/src/App.js
+++ website/src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+const HomePage = () =>{
+  return (
+    <div>hello world</div>
+  );
+}
+
+const AboutPage = () =>{
+  return (
+    <div>hello world 2</div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />}>
+
+      </Route>
+      <Route path="/a" element={<AboutPage />}>
+
+</Route>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
