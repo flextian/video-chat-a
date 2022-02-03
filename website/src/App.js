0142426ab4f84eaf1c3df4ebfcd4819b1165635e
Kahee Tomlinson <48109534+EvaTomlinson@users.noreply.github.com>
2022-02-02 23:33:51 -0500

    added places for the chat box and video box to go

--- website/src/App.js
+++ website/src/App.js
@@ -1,42 +1,24 @@
-import './App.css';
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import OuterBlueBox from './OuterBlueBox';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomePage } from "./App/HomePage";
 
-const HomePage = () =>{
+const AboutPage = () => {
   return (
-    <div className="App">
-    <h1>Hello World!</h1>
-    <OuterBlueBox />
-    </div>
-
-  );
-}
-
-
-const AboutPage = () =>{
-  return (
-    <div className="App">
+    <div>
       <h1>This is the about page</h1>
     </div>
   );
-}
+};
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomePage />}>
-
-      </Route>
-      <Route path="/a" element={<AboutPage />}>
-
-</Route>
-    </Routes>
-  </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/a" element={<AboutPage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ website/src/App/HomePage/ChatBox.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const ChatBox = () => {
+  return <div>Hi! I'm a chat box!</div>;
+};
