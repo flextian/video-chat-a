d1846fe28486b936ced22795c36a46001bd9a53f
Kahee Tomlinson <48109534+EvaTomlinson@users.noreply.github.com>
2022-02-08 19:13:38 -0500

    created a ScreenPage

--- website/src/App.js
+++ website/src/App.js
@@ -2,21 +2,15 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "./App/HomePage";
+import { ScreenPage } from "./App/ScreenPage/ScreenPage";
 
-const AboutPage = () => {
-  return (
-    <div>
-      <h1>This is the about page</h1>
-    </div>
-  );
-};
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
-        <Route path="/a" element={<AboutPage />}></Route>
+        <Route path="/a" element={<ScreenPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ website/src/App/ScreenPage/ScreenPage.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import React from "react";
+
+export const ScreenPage = () => {
+  return (
+    <div>
+      <h1>Screen page</h1>
+    </div>
+  );
+};
