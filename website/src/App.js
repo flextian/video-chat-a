6581a7925819d3e92ccd026b690d61d99b57f9bb
flextian <atian9@gatech.edu>
2022-02-08 19:04:44 -0500

    created start page route

--- website/src/App.js
+++ website/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "./App/HomePage";
+import { StartPage } from "./App/StartPage";
 
 const AboutPage = () => {
   return (
@@ -15,6 +16,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/start" element={<StartPage />}></Route>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/a" element={<AboutPage />}></Route>
       </Routes>
