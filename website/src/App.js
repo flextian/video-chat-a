9e974f965a328dd33128969b993d23665d6425e1
flextian <atian9@gatech.edu>
2022-02-15 01:12:36 -0500

    finished start page

--- website/src/App.js
+++ website/src/App.js
@@ -4,17 +4,36 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "./App/HomePage";
 import { ScreenPage } from "./App/ScreenPage/ScreenPage";
 import { StartPage } from "./App/StartPage";
+import { createTheme, ThemeProvider } from "@mui/material";
 
-
+const mainTheme = createTheme({
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+});
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/start" element={<StartPage />}></Route>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/screenPage" element={<ScreenPage />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={mainTheme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/start" element={<StartPage />}></Route>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/screenPage" element={<ScreenPage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+
   );
 }
 
