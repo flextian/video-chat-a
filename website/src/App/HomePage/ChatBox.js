e446f59a3859986fe9e11769df1d5534fe42dc38
Kahee Tomlinson <48109534+EvaTomlinson@users.noreply.github.com>
2022-02-21 20:47:48 -0500

    worked on interactivity

--- website/src/App/HomePage/ChatBox.js
+++ website/src/App/HomePage/ChatBox.js
@@ -1,17 +1,78 @@
 import React from "react";
 import styled from "styled-components";
+import { useState } from "react";
+
+// const messages = [
+//   { contents: "Hi", senderId: 13 },
+//   { contents: "My name is Eva", senderId: 2 },
+// ];
+const users = [
+  { id: 13, name: "Kurtoo" },
+  { id: 2, name: "Eva" },
+];
+const currentUser = { userId: 2 };
 
 export const ChatBox = () => {
+  const [messages, setMessages] = useState([
+    { contents: "Hi", senderId: 13 },
+    { contents: "My name is Eva", senderId: 2 },
+  ]);
+
+  const onSendClick = (event) => {
+    console.log("clicked!", event);
+    // create a new message
+    const newMessage = {
+      contents: "",
+      senderId: 0,
+    };
+
+    // update messages
+    // clear text box
+  };
+
   return (
     <Column>
-      <Messages>Messages go here!</Messages>
+      <Messages messages={messages} users={users} currentUser={currentUser}>
+        Messages go here!
+      </Messages>
       <Row>
-        <StyledInput></StyledInput>
-        <StyledButton>send</StyledButton>
+        <StyledInput
+          value="yes!"
+          onChange={(event) => console.log(event)}
+        ></StyledInput>
+        <StyledButton onClick={onSendClick}>send</StyledButton>
       </Row>
     </Column>
   );
 };
+
+const Messages = ({ messages, users, currentUser }) => {
+  return (
+    <GrowBox>
+      {messages.map((message) => (
+        <Message
+          message={message}
+          users={users}
+          currentUser={currentUser}
+        ></Message>
+      ))}
+    </GrowBox>
+  );
+};
+const GrowBox = styled.div`
+  flex-grow: 1;
+`;
+
+const Message = ({ message, users, currentUser }) => {
+  const user = users.find((user) => user.id === message.senderId);
+  const userName = user.name;
+  return (
+    <div>
+      {userName}: {message.contents}
+    </div>
+  );
+};
+
 const Column = styled.div`
   display: flex;
   flex-direction: column;
@@ -19,9 +80,6 @@ const Column = styled.div`
   flex-grow: 1;
   margin: 16px;
   padding: 16px;
-`;
-const Messages = styled.div`
-  flex-grow: 1;
 `;
 const Row = styled.div`
   display: flex;
